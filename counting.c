93b61499854dd9d52a497b3e538552b6fce593d5
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-02-07 16:39:28 +0530

    Create counting.c

---
+++ counting.c
@@ -0,0 +1,23 @@
+#include<stdio.h.
+void main()
+{
+int cch=0;
+  int cwd=1;
+char c='a';
+  printf("Enter your sentence in lowercase: ");
+  while(ch!='\r')
+  {
+    ch=getche();
+    if(ch==' ')
+      cwd++;
+    else
+      cch++;
+  }
+
+  printf("\n%dWords = ",cwd);
+
+  printf("%dCharacters = ",cch-1);
+
+  getch();
+
+}
