6a4512d81a4b09128d5d5f26bbd3949a16459a9b
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-02-09 16:46:30 +0530

    Create kishore.c

---
+++ kishore.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int temp,count=0;
+char kh[200];
+printf("\n enter the nummber:");
+gets(kh);
+for(temp=0;d[temp]!=0;temp++)
+{
+if(kh[temp]>=0&&kh[temp]<=9)
+{
+count++;
+}
+}
+printf("the numeric values are %d",temp,count);
+getch();
+}
