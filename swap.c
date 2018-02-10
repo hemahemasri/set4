0e9c81b1d1ad2cc13852a5ddc94c9df454b1342e
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-02-10 20:05:18 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void swap(int,int);
+void main()
+{
+int a, b;
+printf("entere the values:\n");
+scanf("%d%d",&a,&b);
+swap(a,b);
+}
+void swap(int x,int y)
+{
+int t;
+x=t;
+x=y;
+y=t;
+printf("%d swapped"x,y
+getch();
+}
