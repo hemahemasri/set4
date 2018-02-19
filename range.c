264e02cb968f7b65cae0ebc484237bce3f891a57
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-02-19 15:58:06 +0530

    Create range.c

---
+++ range.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter the number to print\n");
+scanf("%d",&n);
+if(n<=10)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+getch();
+}
