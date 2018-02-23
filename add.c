be23173eb139c19d3e22f9304379531d50f87fd8
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-02-23 16:04:19 +0530

    Rename avg.c to add.c

---
+++ add.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a[10],i,n,mark,average;
+clrscr();
+printf("\n enter the total number of subj");
+scanf("%d",&n);
+printf("\n enter the mark ");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+ }
+ mark=a[i];
+ average=mark/n;
+ printf(" the average is=%d ",average);
+ getch();
+ }
+ 
