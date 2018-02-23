03f528ededd055b8cd084ece6425d8c2891d975b
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-02-23 16:19:06 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n;
+clrscr();
+printf("\n enter the number");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("%d is a power of two  ");
+}
+else
+{
+printf("\n %d is not a power of two");
+}
+getch();
+}
