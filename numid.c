74c943636d842fa4c891a2ab8cfee81c12b33759
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-02-28 22:04:12 +0530

    Create numid.c

---
+++ numid.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+	char s[20];
+	int n,i,flag;
+	printf("enter the string:");
+	scanf("%s",s);
+	n=strlen(s);
+	for(i=0;i<n;i++)
+	{
+		if((s[i]>='a'&&s[i]<='z')||(s[i]>='A' && s[i]<='Z'))
+		{
+			flag=1;
+		}
+		else if(s[i]>='0' && s[i]<='9')
+		{
+			flag=2;
+		}
+		else
+		{
+			flag=0;
+		}
+
+	}
+	if(flag==1 && flag==2)
+	{
+		printf("\n yes");
+	}
+	else
+	{
+		printf("\n no");
+	}
+
+	return 0;
+}
