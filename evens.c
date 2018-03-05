0f35a0b7dbabd86ac7a66b005c0406a26250cd63
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-03-05 15:43:34 +0530

    Create evens.c

---
+++ evens.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#inlude<conio.h>
+void main()
+{
+int a,b;
+clrscr();
+printf("\n enter the numbers");
+scanf("%d%d",&a,&b);
+if((a%2==0)&&(b%2==0))
+{
+printf("\n even number");
+}
+else
+{
+printf("\n odd number");
+}
+getch();
+}
