eae805dbd8190fd34fe43d41d1fbafb72f53d13b
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-03-05 16:20:53 +0530

    Create 7.c

---
+++ 7.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+clrscr();
+printf("\n enter the number");
+scanf("%d",&i);
+if(i%7==0)
+{
+printf("\n yes it is multiple of 7");
+}
+else
+{
+printf("\n no it is not a multiple of 7");
+}
+getch();
+}
