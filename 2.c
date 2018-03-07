51d937b990e4bb681a1703e1360f6f2a8932542b
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-03-07 15:56:53 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int h,k;
+clrscr();
+cout<<"\n enter the number";
+cin>>h;
+for(k=0;k<h;k++)
+{
+k=h*2;
+}
+cout<<"\n the multiple of 2 is"<<k;
+getch();
+}
