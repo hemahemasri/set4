433eb4e4eea4db764803853ca03a7dcf72bcb0a9
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-03-12 16:07:43 +0530

    Create pinky.c

---
+++ pinky.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+float n;
+int h;
+pritnf("\n entre the no");
+scanf("%f",&n);
+h=(int)(n+0.5);
+printf("\n %d",h);
+getch();
+}
