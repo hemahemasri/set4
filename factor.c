bc8b9db56c2d775fa8de3c9378e016030126215c
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-03-12 16:53:31 +0530

    Create factor.c

---
+++ factor.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int m,h;
+clrscr();
+printf("\n enter the number");
+scanf("%d",&h);
+for(m=1;m<=n;m++)
+{
+if(h%m==0)
+printf("\n%d",m);
+}
+getch();
+}
