c89504d37cb40ea7b4be8479ecf366903d5718b8
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-03-14 16:53:16 +0530

    Create y.c

---
+++ y.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a1,a2,a,n,n1,n2,m,m1,m2;
+clrscr();
+printf("\n enter the number s");
+scanf("%d%d",&a1,&a2);
+printf("\n enter the next ninjas");
+scanf("%d%d",&n1,&n2);
+printf("\n enter the next ninjas");
+scanf("%d%d",&m1,&m2);
+a=a1-a2;
+n=n1-n2;
+m=m1-m2;
+printf("\n%d",a);
+printf("\n%d",n);
+printf("\n%d",m);
+getch();
+}
