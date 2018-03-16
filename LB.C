1c92c4114b7543e9f9bd22254bdaf76c562d2f73
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-03-16 15:43:33 +0530

    Create LB.C

---
+++ LB.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int l,b,a;
+clrscr();
+printf("\n enter the length");
+scanf("%d",&l);
+printf("\n enter the breadth");
+scanf("%d",&b);
+a=l*b;
+printf(" area is %d",a);
+getch();
+}
