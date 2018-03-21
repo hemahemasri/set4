428cf73eb0bc96a1225083b47dbab75adeea2b73
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-03-21 21:17:22 +0530

    Create in.c

---
+++ in.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,a[20];
+printf("\n enter the number ");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",a[i]);
+}
+if(i!=a[i])
+{
+printf("%d",i);
+}
+getch();
+}
