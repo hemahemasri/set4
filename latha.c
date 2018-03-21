64cf3ad973afab8493d06f938c14e354fd4d3af2
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-03-21 15:46:18 +0530

    Create latha.c

---
+++ latha.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,sum;
+printf("\n enter the values");
+scanf("%d%d%d",&a,&b,&c);
+for(i=0;i<n;i++)
+{
+sum= a+b+c;
+}
+printf("%d",sum);
+getch();
+}
