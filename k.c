20496111381d428f04961477f0c17b3bc96c5b3f
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-03-21 16:29:17 +0530

    Create k.c

---
+++ k.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+inti a,k,a1[100];
+printf("\n enter the number ");
+scanf("%d%d",&a,&k);
+for(i=0;i<a;i++)
+{
+scanf("%d",a1[i]);
+}
+for(i=0;i<a;i++)
+{
+while(i==k)
+{
+printf("%d",a[i]);
+}
+}
