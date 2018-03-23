1c4ef3e655e65884cca0546707e5a0f39d94887c
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-03-23 16:46:12 +0530

    Create raja.c

---
+++ raja.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d,e;
+clrscr();
+printf("\n enter the number");
+scanf("%d%d%d",&a,&b,&c);
+d=a*b;
+e=d/c;
+printf("\n %d",e);
+getch();
+}
