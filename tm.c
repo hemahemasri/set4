db16a2b0781d781eb63ccde3d56a1d0e9a806c62
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-03-25 12:17:50 +0530

    Create tm.c

---
+++ tm.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int i,n,sum;
+clrscr();
+printf("Enter the number");
+scanf("%d",&a);
+for(i=0;i<a;i++)
+{
+sum=sum+i;
+}
+printf("The sum of natural number is..:%d",sum);
+getch();
+}
