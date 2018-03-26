3afe07042b4f1616368383c38600350627c6c398
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-03-26 16:18:45 +0530

    Create ass.c

---
+++ ass.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,temp,reverse=0,remainder;
+printf("\n enter the number");
+scanf("%d",&a);
+temp=a;
+while(a!=0)
+{
+remainder=n%10;
+reverse=reverse*10+remainder;
+n/=10;
+}
+ printf("\n %d reverse is =d",reverse );
+ getch();
+ }
+ 
