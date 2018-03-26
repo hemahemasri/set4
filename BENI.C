269f03d44465383ed5f84a53b8a046130b076858
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-03-26 16:49:09 +0530

    Update BENI.C

--- BENI.C
+++ BENI.C
@@ -4,24 +4,24 @@
 void main()
 {
 int n,i,res,a[20],t=0;
-char r[10];
+char h[10];
 clrscr();
 printf("enter roman number:");
-scanf("%s",r);
-n=strlen(r);
+scanf("%s",h);
+n=strlen(h);
 for(i=0;i<n;i++)
 {
-if(r[i]=='I')
+if(h[i]=='I')
 a[i]=1;
-else if(r[i]=='V')
+else if(h[i]=='V')
 a[i]=5;
-else if(r[i]=='X')
+else if(h[i]=='X')
 a[i]=10;
-else if(r[i]=='L')
+else if(h[i]=='L')
 a[i]=50;
-else if(r[i]=='C')
+else if(h[i]=='C')
 a[i]=100;
-else if(r[i]=='D')
+else if(h[i]=='D')
 a[i]=500;
 else
 printf("\nINVALID");
